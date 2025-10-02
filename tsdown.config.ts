878bd01a628fac573efdbea7cdd74ba41f951017
Coder-King3 <2196592083@qq.com>
2025-10-02 21:31:51 +0800

    :gear: confg: change configs

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,23 @@
+import type { UserConfig } from 'tsdown'
+
 import { defineConfig } from 'tsdown'
 
-export default defineConfig({
-  entry: ['src/index.ts'],
-  shims: true,
-  format: ['esm', 'cjs']
-})
+const createConfigs = (base: UserConfig, configs: UserConfig[]) => {
+  const config = configs.map((config) => ({
+    ...base,
+    ...config
+  })) as UserConfig
+  return config
+}
+
+const baseConfig: UserConfig = {
+  entry: ['./src/index.ts'],
+  clean: true
+}
+
+export default defineConfig(
+  createConfigs(baseConfig, [
+    { format: 'esm', dts: true },
+    { format: 'cjs', dts: false }
+  ])
+)
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    testTimeout: 60_000
+    // timeout 10000
+    testTimeout: 10000,
+
+    // watch false
+    watch: false
   }
 })
